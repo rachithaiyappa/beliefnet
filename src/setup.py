ef24e19eebc6f7b675897cfaf7ada840e3f0d30c
rachithaiyappa <rachithaiyappa96@gmail.com>
2023-03-12 21:13:47 -0400

    package

---
+++ src/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name="beliefnet",
+    version="0.1",
+    description="belief network package",
+    author="Rachith Aiyappa <rachithaiyappa96@gmail.com> and Yong-Yeol Ahn <yyahn@iu.edu>",
+)
